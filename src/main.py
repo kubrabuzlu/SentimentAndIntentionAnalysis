22cf11424c6108442215d6da88824e75235e6ff9
kubrabuzlu <kubraa.buzlu@gmail.com>
2024-05-12 00:00:50 +0300

    feat(main): update functionality in main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
+import configparser
+
 print("Sentiment and Intention Analysis Project")
+
+# Config dosyasını okuma
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# Modelin adını al
+model_name = config["model"]["name"]
+
+# Sentiment etiketlerini al
+sentiment_labels = ["positive", "negative", "neutral"]
+
+# Intention etiketlerini al
+intention_labels = ["Inquire","Information Gathering", "Payment", "Trade", "Customer Dissatisfaction",
+                    "Customer Satisfaction", "Closing Remarks", "Change Package", "Upgrade", "Purchase"]
+
+data_path = config["data"]["data_dir"]
+
