9ec08134ecd043aee4900f1f3435a67f7f289fb0
kubrabuzlu <kubraa.buzlu@gmail.com>
2024-05-12 00:57:43 +0300

    feat: update functionality in main.py and SentimentAndIntentionAnalysis.py

--- src/SentimentAndIntentionAnalysis.py
+++ src/SentimentAndIntentionAnalysis.py
@@ -1,5 +1,8 @@
 from transformers import pipeline
 class ZeroShotClassifier:
+
+    def __init__(self, model_name):
+        self.model = self.create_model(model_name)
     def create_model(self, model_name):
         classifier = pipeline("zero-shot-classification", model=model_name)
         return classifier
--- src/main.py
+++ src/main.py
@@ -1,20 +1,45 @@
 import configparser
-
+import json
+from SentimentAndIntentionAnalysis import ZeroShotClassifier
+from transformers import pipeline
 print("Sentiment and Intention Analysis Project")
 
-# Config dosyasını okuma
-config = configparser.ConfigParser()
-config.read("config.ini")
-
-# Modelin adını al
-model_name = config["model"]["name"]
-
-# Sentiment etiketlerini al
+# Sentiment Labels
 sentiment_labels = ["positive", "negative", "neutral"]
 
-# Intention etiketlerini al
+# Intention Labels
 intention_labels = ["Inquire","Information Gathering", "Payment", "Trade", "Customer Dissatisfaction",
-                    "Customer Satisfaction", "Closing Remarks", "Change Package", "Upgrade", "Purchase"]
+                        "Customer Satisfaction", "Closing Remarks", "Change Package", "Upgrade", "Purchase"]
 
-data_path = config["data"]["data_dir"]
+def get_config():
 
+    # Read config
+    config = configparser.ConfigParser()
+    config.read(r"C:\Users\kbuzlu\PycharmProjects\SentimentIntentionAnalysis\config\config.ini")
+
+    # Get model_name
+    model_name = config["model"]["name"]
+
+    # Get data path
+    data_path = config["data"]["data_dir"]
+    return model_name, data_path
+
+def load_data(data_path):
+    with open(data_path, "r") as file:
+        data = json.load(file)
+    texts = [entry["text"] for entry in data]
+    return texts
+
+if __name__ == "__main__":
+    model_name, data_path = get_config()
+    data = load_data(data_path)
+    create_classifier = ZeroShotClassifier(model_name)
+    classifier = create_classifier.model
+    for sentence in data:
+        sentiment_result = classifier(sentence, sentiment_labels)
+        print(f"\nSentence: {sentence}")
+        print("Sentiment:", sentiment_result["labels"][0])
+
+        # Intention analysis
+        intention_result = classifier(sentence, intention_labels)
+        print("Intention:", intention_result["labels"][0])
