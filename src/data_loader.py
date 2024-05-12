92675b9a3117efafcc3ef91836afa4bf95052827
kubrabuzlu <kubraa.buzlu@gmail.com>
2024-05-12 16:30:18 +0300

    feat(src): add data_loader.py

---
+++ src/data_loader.py
@@ -0,0 +1,29 @@
+import configparser
+import json
+def get_config():
+    # Read config
+    config = configparser.ConfigParser()
+    config.read(r"C:\Users\kbuzlu\PycharmProjects\SentimentIntentionAnalysis\config\config.ini")
+
+    # Get model_name
+    model_name = config["model"]["name"]
+
+    # Get data path
+    data_path = config["data"]["data_dir"]
+
+    # Get possible labels
+    labels_path = config["data"]["possible_labels_dir"]
+    return model_name, data_path, labels_path
+
+def load_data(data_path):
+    with open(data_path, "r") as file:
+        data = json.load(file)
+    texts = [entry["text"] for entry in data]
+    return texts
+
+def load_labels(labels_path):
+    with open(labels_path, "r") as file:
+        data = json.load(file)
+    sentiment_labels = data["sentiment_labels"]
+    intention_labels = data["intention_labels"]
+    return sentiment_labels, intention_labels
