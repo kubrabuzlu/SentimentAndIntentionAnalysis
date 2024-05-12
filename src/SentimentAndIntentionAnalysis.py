a5dac374336d3d4b4283cb736c113f8e7ed86957
kubrabuzlu <kubraa.buzlu@gmail.com>
2024-05-12 13:51:10 +0300

    feat: update functionality in SentimentAndIntentionAnalysis.py

--- src/SentimentAndIntentionAnalysis.py
+++ src/SentimentAndIntentionAnalysis.py
@@ -1,8 +1,10 @@
-from transformers import pipeline
+from transformers import pipeline, BartTokenizer, BartForSequenceClassification
 class ZeroShotClassifier:
 
     def __init__(self, model_name):
         self.model = self.create_model(model_name)
     def create_model(self, model_name):
-        classifier = pipeline("zero-shot-classification", model=model_name)
+        tokenizer = BartTokenizer.from_pretrained(model_name)
+        model = BartForSequenceClassification.from_pretrained(model_name)
+        classifier = pipeline("zero-shot-classification", model=model, tokenizer=tokenizer)
         return classifier
