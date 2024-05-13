73b8e7a5b1e6d3c0bd3a3a442fb249b8e4d1468a
kubrabuzlu <kubraa.buzlu@gmail.com>
2024-05-13 03:10:45 +0300

    feat(api-version): update api.py

--- api-version/api/api.py
+++ api-version/api/api.py
@@ -1,25 +1,14 @@
 import configparser
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from src.SentimentAndIntentionAnalysis import ZeroShotClassifier
-from src.data_loader import *
+from SentimentAndIntentionAnalysis import ZeroShotClassifier
 
 # Initialize FastAPI app
 app = FastAPI()
-app.add_middleware(CORSMiddleware,
-                   allow_origins=["http://localhost:8501"],
-                   allow_credentials=True,
-                   allow_methods=["*"],
-                   allow_headers=["*"],)
-#Get model_name, data_path and labels_path
-model_name, data_path, labels_path = get_config()
 
-# Load sentiment labels and intention labels
-sentiment_labels, intention_labels = load_labels(labels_path)
 # Create Analzer
-analyzer = ZeroShotClassifier(model_name=model_name, sentiment_labels=sentiment_labels, intention_labels=intention_labels)
+analyzer = ZeroShotClassifier(model_name='facebook/bart-large-mnli')
 
 class AnalysisResult(BaseModel):
     sentiment: str
@@ -32,9 +21,3 @@ class Text(BaseModel):
 def analyze_text(data: Text):
     result = analyzer.analyze_text(data.text)
     return AnalysisResult(sentiment=result["sentiment"], intention=result["intention"])
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("api-version:app", host="0.0.0.0", port=8000, reload=True)
-
-
