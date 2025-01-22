2d23e78aa8e849e340a3501c291a4aaad3b5163f
kubrabuzlu <kubraa.buzlu@gmail.com>
2025-01-22 13:55:47 +0300

    perf: normalize file paths for cross-platform compatibility

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,12 @@
 import configparser
 import json
+import os
+
+
 def get_config():
     # Read config
     config = configparser.ConfigParser()
-    config.read(r"..\config\config.ini")
+    config.read(os.path.join("..", "config", "config.ini"))
 
     # Get model_name
     model_name = config["model"]["name"]
@@ -15,12 +18,17 @@ def get_config():
     labels_path = config["data"]["possible_labels_dir"]
     return model_name, data_path, labels_path
 
+
 def load_data(data_path):
+    # Normalize the path for cross-platform compatibility
+    normalized_data_path = os.path.normpath(data_path)
+
     # Load data
     with open(data_path, "r") as file:
         data = json.load(file)
     texts = [entry["text"] for entry in data]
     return texts
+
 
 def load_labels(labels_path):
     # Load labels
